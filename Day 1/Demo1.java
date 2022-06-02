eb31ff5790689ce41cf2d6fb43598276621c49a1
20211420181137010024 <jayshreepradhan12345@gmail.com>
2022-06-02 12:24:52 +0530

    initial commit

---
+++ Day 1/Demo1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Demo1{
+public static void main(String args[])
+{
+    System.out.println("Hello World");
+    System.out.println("Hello Aurangabad");
+    System.out.println("Hello To All ");
+    System.out.println("Hello Jayshree");
+    }
+}
---
+++ Day 1/demo4.java
@@ -0,0 +1,13 @@
+//WAP to convert meter to feet & meter to inches
+import java.util.Scanner;
+class demo4
+{
+public static void main(String[] args) 
+ {
+  Scanner input = new Scanner(System.in);
+  System.out.print("Enter Value in Meters :");
+  double meter = input.nextDouble();
+  double feet = meter * 3.28084;
+  System.out.print(meter + " Meters = "+ feet + " Feets");
+ }
+ }
